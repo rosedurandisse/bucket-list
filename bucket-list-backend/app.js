004da63a33e70a98c0143e815e85caf847512c2e
Rose Durandisse <rosedurandisse@pursuit.org>
2023-01-12 16:05:13 -0500

    Created a express backend with two basic routes

---
+++ bucket-list-backend/app.js
@@ -0,0 +1,19 @@
+//this is the dream of my kitchen
+const express = require("express");
+
+//this is using the tools to create my kitchen
+const app = express();
+
+//converts the ingredients to meals can eat/understnad
+app.use(express.json());
+
+//import activity
+const adventuresController = require("./controllers/adventuresController");
+
+app.use("/adventures", adventuresController);
+
+app.get("/", (request, response) => {
+  response.json({ message: "Welcome to the Bucket List" });
+});
+
+module.exports = app;
